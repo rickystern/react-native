e51aa98bd0655c06818558390feddee754c8eb26
Rohan Stern <rstern@realdecoy.com>
2019-07-03 15:34:48 -0500

    location shows

--- ReactFlutterApp/services/ProductLocations.js
+++ ReactFlutterApp/services/ProductLocations.js
@@ -3,17 +3,30 @@ import { Platform, StyleSheet, Text, View } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 export default
 class App extends React.Component {
+
+
+
   render() {
     return (
       <MapView
          style={{ flex: 1 }}
-         provider={PROVIDER_GOOGLE}
-         showsUserLocation
+        // provider={PROVIDER_GOOGLE}
+         showsUserLocation={true}
+		 showsMyLocationButton={true}
+		 showsCompass={true}
+		 followsUserLocation={true}
+		 loadingEnabled={true}
+		 toolbarEnabled={true}
+		 zoomEnabled={true}
+         rotateEnabled={true}
+                    
          initialRegion={{
          latitude: 18.109581,
          longitude: -77.297508,
          latitudeDelta: 0.0922,
-         longitudeDelta: 0.0421}}
+         longitudeDelta: 0.0421
+        
+        }}
       />
     );
   }
