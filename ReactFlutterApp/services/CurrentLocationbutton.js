84d369d22136f0ed2004285ebf144c941f057546
Rohan Stern <rstern@realdecoy.com>
2019-07-04 12:48:29 -0500

    added new map functionality

---
+++ ReactFlutterApp/services/CurrentLocationbutton.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {StyleSheet, Text, View, Dimensions, TouchableOpacity, Alert} from 'react-native';
+//import {Materialicons} from 'expo/vector-icons';
+import Icon from '@expo/vector-icons/FontAwesome';
+
+
+const WIDTH = Dimensions.get('window').width;
+const HEIGHT= Dimensions.get('window').height;
+
+export const CurrentLocationButton = function (props){
+
+    const bottom = props.bottom? props.bottom: 65;
+
+     return(
+
+         <View style ={[styles.container , {top: HEIGHT- bottom}]}>
+
+                        <Icon 
+                        name= "my-location"
+                        color="black"
+                        size={25}
+                        onPress ={()=>{}}
+                        />
+
+
+
+         </View>
+     )
+}
+
+const styles = StyleSheet.create({
+    conatainer:{
+        zIndex:9,
+        position: 'absolute',
+        width: 45,
+        height: 45,
+        backgroundColor: '#fff',
+        left: WIDTH - 70,
+        borderRadius: 50,
+        shadowColor: '#000000',
+        elevation: 7,
+        shadowOpacity: 1.0,
+        justifyContent: 'space-around',
+        alignItems: 'center'
+    }
+})
--- ReactFlutterApp/services/ProductLocations.js
+++ ReactFlutterApp/services/ProductLocations.js
@@ -1,33 +1,76 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { Location} from 'expo';
+import * as Permissions from 'expo-permissions'
+import {DestinationButton} from './DestinationButton';
+//import {CurrentLocationButton} from './CurrentLocationbutton'
+
+
+
+
 export default
 class App extends React.Component {
 
+    constructor(props){
+        super(props);
+        
+        this.state={
+            region:null
+    }
+    this._getLocationAsync();
+}
 
+    _getLocationAsync = async () => {
+        let {status} = await Permissions.askAsync(Permissions.LOCATION);
+            if(status !== 'granted')
+            console.log('Permission to location was denied ');
+        let location = await location.getCurrentPositionAsync({enabledHighAccuracy: true});
+        let region = {
+
+            latitude:location.coords.latitude,
+            longitude:location.coords.longitude,
+            latitudeDelta: 0.045,
+            longitudeDelta:0.045,
+        }
+        this.setState({ region: region})
+    }
 
   render() {
+     
     return (
-      <MapView
-         style={{ flex: 1 }}
-        // provider={PROVIDER_GOOGLE}
-         showsUserLocation={true}
-		 showsMyLocationButton={true}
-		 showsCompass={true}
-		 followsUserLocation={true}
-		 loadingEnabled={true}
-		 toolbarEnabled={true}
-		 zoomEnabled={true}
-         rotateEnabled={true}
+        <View style = {styles.container}>
+
+             <DestinationButton />
+             {/* <CurrentLocationButton/> */}
+                <MapView
+                    initialRegion={this.state.region}
+                    style={{ flex: 1 }}
+                    // provider={PROVIDER_GOOGLE}
+                    showsUserLocation={true}
+                    showsMyLocationButton={true}
+                    showsCompass={true}
+                    followsUserLocation={true}
+                    loadingEnabled={true}
+                    toolbarEnabled={true}
+                    zoomEnabled={true}
+                    rotateEnabled={true}
+                                
                     
-         initialRegion={{
-         latitude: 18.109581,
-         longitude: -77.297508,
-         latitudeDelta: 0.0922,
-         longitudeDelta: 0.0421
-        
-        }}
-      />
+                />
+       
+
+        </View>
+             
+
+      
     );
   }
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: '#fff'
+    }
+})
